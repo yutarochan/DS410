62cef21bcc645b2ede338f888d6e11744d123468
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-04-14 13:31:00 -0400

    (fix) Change parameter for execution

--- Development/feature_modeling/execute.sh
+++ Development/feature_modeling/execute.sh
@@ -8,6 +8,6 @@ for i in "${nodes[@]}"
 do
     for j in "${cores[@]}"
     do
-        spark-submit --master yarn-client --driver-memory 15g --executor-memory 15g --num-executors $i --executor-cores $j target/scala-2.11/feature_modeling_2.11-0.1-SNAPSHOT.jar
+        spark-submit --master yarn-client --driver-memory 15g --executor-memory 15g --num-executors $i --executor-cores $j target/scala-2.11/feature_modeling_2.11-0.1-SNAPSHOT.jar tf-idf
     done
 done
