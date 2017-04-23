1155e4179c5698d65132d1a94081a4a73585ba3e
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-04-23 19:57:50 -0400

    (fix) Append persistence parameter

--- Development/review_predict/execute.sh
+++ Development/review_predict/execute.sh
@@ -2,16 +2,12 @@
 # Executor Parameter Space
 nodes=( 2 3 4 5 )
 cores=( 2 4 8 )
-memory=( 5 10 15 )
 
 # Iterate Executions
 for i in "${nodes[@]}"
 do
     for j in "${cores[@]}"
     do
-        for k in "${cores[@]}"
-        do
-            spark-submit --master yarn-client --driver-memory $k g --executor-memory 15g --num-executors $i --executor-cores $j target/scala-2.11/review_predict_2.11-0.1-SNAPSHOT.jar
-        done
+        spark-submit --master yarn-client --driver-memory 10g --executor-memory 15g --num-executors $i --executor-cores $j target/scala-2.11/review_predict_2.11-0.1-SNAPSHOT.jar
     done
 done
