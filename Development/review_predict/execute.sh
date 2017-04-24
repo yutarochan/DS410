84a46dbca7284f168368689fcc1c3f08252a786a
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-04-24 19:47:03 -0400

    (add) Modify shell script

--- Development/review_predict/execute.sh
+++ Development/review_predict/execute.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # Executor Parameter Space
-nodes=( 2 3 4 5 )
+nodes=( 3 4 5 )
 cores=( 2 4 8 )
+mem=( 2 5 10 )
 
 # Iterate Executions
 for i in "${nodes[@]}"
 do
     for j in "${cores[@]}"
     do
-        spark-submit --master yarn-client --driver-memory 10g --executor-memory 15g --num-executors $i --executor-cores $j target/scala-2.11/review_predict_2.11-0.1-SNAPSHOT.jar
+        for k in "${mem[@]}"
+        do
+            spark-submit --master yarn-client --driver-memory $k\G --executor-memory $k\G --num-executors $i --executor-cores $j target/scala-2.11/review_predict_2.11-0.1-SNAPSHOT.jar
+        done
     done
 done
