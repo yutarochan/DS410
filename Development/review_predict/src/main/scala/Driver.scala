72dbf26858523f7e3a88850e4d58cfa61ffdcc1c
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-04-25 09:47:26 -0400

    (add) Append partitioning

--- Development/review_predict/src/main/scala/Driver.scala
+++ Development/review_predict/src/main/scala/Driver.scala
@@ -32,14 +32,14 @@ object ReviewPredict {
     def main(args: Array[String]): Unit = {
         // Import HDFS and Parse JSON Object
         val reviews = sc.textFile("hdfs:/user/yjo5006/reviews_Books_5.json.gz")
-        val review_df = sqlContext.read.json(reviews).repartition(5)
+        val review_df = sqlContext.read.json(reviews)
 
         // ID to Integer Mapping - Map Between Integer to String ID (vice-versa)
 		val user_int = review_df.select("reviewerID").rdd.map(x=> (x(0).toString, 1) ).groupByKey.map(x=>(x._1,x._2.sum)).filter(_._2 > 14).map(x=>x._1).distinct().zipWithUniqueId()
 		val prod_int = review_df.select("asin").rdd.map(x=> (x(0).toString,1) ).groupByKey.map(x=>(x._1,x._2.sum)).filter(_._2 > 24).map(x=>x._1).distinct().zipWithUniqueId()
 
         // Process Integer Mapping
-        val ratings = review_df.select("reviewerID", "asin", "overall").rdd.map(x => (x(0).toString, x(1).toString, x(2).toString.toDouble))
+        val ratings = review_df.select("reviewerID", "asin", "overall").rdd.map(x => (x(0).toString, x(1).toString, x(2).toString.toDouble)).partitionBy(new HashPartition(100)).persist(MEMORY_AND_DISK_SER)
         val ratings_user = ratings.keyBy(_._1).join(user_int).map(x => (x._2._1._1, x._2._1._2, x._2._2.toInt, x._2._1._3))                         // (uid_str, pid_str, uid_int, rating)
         val ratings_data = ratings_user.keyBy(_._2).join(prod_int).map(x => (x._2._1._1, x._2._1._2, x._2._1._3.toInt, x._2._2.toInt, x._2._1._4))  // (uid_str, pid_str, uid_str, pid_str, rating)
 
