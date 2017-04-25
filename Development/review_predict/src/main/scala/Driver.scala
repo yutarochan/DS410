aa183f9fcec162a86c8e83172bcab76da51e3098
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-04-25 06:05:29 -0400

    (add) Persistence Update

--- Development/review_predict/src/main/scala/Driver.scala
+++ Development/review_predict/src/main/scala/Driver.scala
@@ -48,7 +48,7 @@ object ReviewPredict {
         // Split Dataset
         val splits = ratings_data.randomSplit(Array(0.7, 0.3))
         // val train = splits(0).map(x => Rating(x._3, x._4, x._5))
-		val train = splits(0).map(x => Rating(x._3, x._4, x._5)).persist(StorageLevel.MEMORY_AND_DISK)
+		val train = splits(0).map(x => Rating(x._3, x._4, x._5)).persist(StorageLevel.DISK_ONLY)
         val test = splits(1).map(x => Rating(x._3, x._4, x._5))
 
         // Build the recommendation model using ALS
