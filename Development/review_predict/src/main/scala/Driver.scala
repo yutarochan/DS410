917537f8e0c50de4cebc84ba7c04bb1aced46e6c
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-04-25 05:21:15 -0400

    (add) Persistence Update

--- Development/review_predict/src/main/scala/Driver.scala
+++ Development/review_predict/src/main/scala/Driver.scala
@@ -32,7 +32,7 @@ object ReviewPredict {
     def main(args: Array[String]): Unit = {
         // Import HDFS and Parse JSON Object
         val reviews = sc.textFile("hdfs:/user/yjo5006/reviews_Books_5.json.gz")
-        val review_df = sqlContext.read.json(reviews)
+        val review_df = sqlContext.read.json(reviews).persist()
 
         // ID to Integer Mapping - Map Between Integer to String ID (vice-versa)
 		val user_int = review_df.select("reviewerID").rdd.map(x=> (x(0).toString, 1) ).groupByKey.map(x=>(x._1,x._2.sum)).filter(_._2 > 14).map(x=>x._1).distinct().zipWithUniqueId()
@@ -43,10 +43,12 @@ object ReviewPredict {
         val ratings_user = ratings.keyBy(_._1).join(user_int).map(x => (x._2._1._1, x._2._1._2, x._2._2.toInt, x._2._1._3))                         // (uid_str, pid_str, uid_int, rating)
         val ratings_data = ratings_user.keyBy(_._2).join(prod_int).map(x => (x._2._1._1, x._2._1._2, x._2._1._3.toInt, x._2._2.toInt, x._2._1._4))  // (uid_str, pid_str, uid_str, pid_str, rating)
 
+		review_df.unpersist()
+
         // Split Dataset
         val splits = ratings_data.randomSplit(Array(0.7, 0.3))
         // val train = splits(0).map(x => Rating(x._3, x._4, x._5))
-		val train = splits(0).map(x => Rating(x._3, x._4, x._5)).persist()
+		val train = splits(0).map(x => Rating(x._3, x._4, x._5))
         val test = splits(1).map(x => Rating(x._3, x._4, x._5))
 
         // Build the recommendation model using ALS
