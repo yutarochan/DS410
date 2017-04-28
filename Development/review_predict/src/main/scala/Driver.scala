ea201dddbcedf7214e3ab04e9287e2da3b11d883
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-04-28 13:43:40 -0400

    (add) Finalize program, ugh

--- Development/review_predict/src/main/scala/Driver.scala
+++ Development/review_predict/src/main/scala/Driver.scala
@@ -32,10 +32,7 @@ object ReviewPredict {
     def main(args: Array[String]): Unit = {
         // Import HDFS and Parse JSON Object
         val reviews = sc.textFile("hdfs:/user/yjo5006/reviews_Books_5.json.gz")
-        val review_df = sqlContext.read.json(reviews).persist()
-
-		val metadata = sc.textFile("hdfs:/user/yjo5006/meta_Books.json.gz").map(x => x.replace("\'", "\""))
-		val metadata_df = sqlContext.read.json(metadata)
+        val review_df = sqlContext.read.json(reviews).persist(StorageLevel.MEMORY_AND_DISK_SER)
 
         // ID to Integer Mapping - Map Between Integer to String ID (vice-versa)
 		val user_int = review_df.select("reviewerID").rdd.map(x=> (x(0).toString, 1) ).groupByKey.map(x=>(x._1,x._2.sum)).filter(_._2 > 14).map(x=>x._1).distinct().zipWithUniqueId()
@@ -45,11 +42,7 @@ object ReviewPredict {
         val ratings = review_df.select("reviewerID", "asin", "overall").rdd.map(x => (x(0).toString, x(1).toString, x(2).toString.toDouble))
         val ratings_user = ratings.keyBy(_._1).join(user_int).map(x => (x._2._1._1, x._2._1._2, x._2._2.toInt, x._2._1._3))                         // (uid_str, pid_str, uid_int, rating)
         val ratings_data = ratings_user.keyBy(_._2).join(prod_int).map(x => (x._2._1._1, x._2._1._2, x._2._1._3.toInt, x._2._2.toInt, x._2._1._4))  // (uid_str, pid_str, uid_str, pid_str, rating)
-
 		review_df.unpersist()
-		ratings_data.persist(StorageLevel.MEMORY_AND_DISK_SER)
-
-		println("[TRAINING MODE]")
 
 	    // Split Dataset
 	    val splits = ratings_data.randomSplit(Array(0.7, 0.3))
@@ -75,6 +68,7 @@ object ReviewPredict {
 	    println("Mean Squared Error = " + MSE)
 
 		// Generate Recommendations
+		/*
 		val prod_hist = usersProducts.filter(_._1 == 1)
 		val rec = model.predict(prod_hist).collect().sortBy(- _.rating).take(25).map(_.product).toSeq
 		val rec_asin = prod_int.filter(rec.contains(_)).collect().toSeq
@@ -89,6 +83,7 @@ object ReviewPredict {
       		// println("%2d".format(i) + ": " + r.product)
       		i += 1
     	}
+		*/
 
 		// Save and load model
 		model.save(sc, "amazon_cf_model")
